2b49bfdbecdcd269046d09e76f83ef7374b14147
JhordanReyes <reyesjhordan4@gmail.com>
2026-04-14 11:14:14 -0500

    Estructura principal de la app, funciones de camara basicas y vista principal

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,52 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Text } from "react-native";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function TabsLayout() {
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#fff",
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        tabBarInactiveTintColor: "rgb(236 254 255 / 0.5)",
+        tabBarStyle: {
+          backgroundColor: "#1f2937"
+        }
+      }}
+    >
+      <Text className="text-cyan-50/50">dw</Text>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Camera",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="camera" size={size} color={color} />
+          ),
         }}
       />
+
       <Tabs.Screen
-        name="explore"
+        name="plans"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Plans",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="star" size={size} color={color} />
+          ),
         }}
       />
+
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+
     </Tabs>
   );
 }
---
+++ app/(tabs)/plans.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function PlansScreen() {
+  return (
+    <View>
+      <Text>Planes</Text>
+    </View>
+  );
+}
